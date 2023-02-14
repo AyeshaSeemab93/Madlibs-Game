0a24ad359e6a619cad23a9fe3e0bb6350452485d
Ayesha Seemab <ayeshaseemab2017@gmail.com>
2023-02-14 21:46:26 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char color[20];
+    char PluralNoun[20];
+    char Celebrity[20];
+     printf("Enter Color: ");
+     scanf("%s", color);
+     printf("Enter Plural Noun: ");
+     scanf("%s", PluralNoun);
+     printf("Enter Celebrity Name: ");
+     scanf("%s", Celebrity);
+
+    printf("Roses are %s\n", color);
+    printf("%s are blue\n", PluralNoun);
+    printf("I love %s.\n", Celebrity);
+
+    return 0;
+}
