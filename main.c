e06e3d0f8f044dfa6621046babba069f9b6ee7b2
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2023-11-14 09:56:03 +0200

    compiler file

--- main.c
+++ main.c
@@ -7,12 +7,14 @@ int main()
     char color[20];
     char PluralNoun[20];
     char Celebrity[20];
-     printf("Enter Color: ");
-     scanf("%s", color);
-     printf("Enter Plural Noun: ");
-     scanf("%s", PluralNoun);
-     printf("Enter Celebrity Name: ");
-     scanf("%s", Celebrity);
+    printf("Enter Color: ");
+    scanf("%s", color);
+    printf("Enter Plural Noun: ");
+    scanf("%s", PluralNoun);
+    printf("Enter Celebrity Name: ");
+    scanf("%s", Celebrity);
+
+    printf("-----------------------------");
 
     printf("Roses are %s\n", color);
     printf("%s are blue\n", PluralNoun);
